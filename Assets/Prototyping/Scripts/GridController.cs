fdd2c3e765f32364cc20d31c11fc35b2623f53ed
viniciussousav <vsv@cin.ufpe.br>
2022-04-08 02:14:53 -0300

    Mecanica de construcao de caminho base

--- Assets/Prototyping/Scripts/GridController.cs
+++ Assets/Prototyping/Scripts/GridController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
@@ -7,17 +9,19 @@ namespace Prototyping.Scripts
 {
     public class GridController : MonoBehaviour
     {
-        
-        [SerializeField] private int tilesLimit;
-        [SerializeField] private int tilesCount;
         [SerializeField] private Text tilesCountTxt;
-        
+        [SerializeField] public Button startWaveBtn;
+
         [SerializeField] private Tilemap pathTileMap;
         [SerializeField] private Tile tile;
 
+        [SerializeField] private int tilesLimit;
+        [SerializeField] private List<Tile> path;
+        [SerializeField] private List<Vector3Int> tilesPositions;
+        
         private void Start()
         {
-            tilesCount = 0;
+            path = new List<Tile>();
             UpdateAvailableTilesText();
         }
 
@@ -25,33 +29,86 @@ namespace Prototyping.Scripts
         {
             if (Input.GetMouseButtonUp(0))
                 SetTileAtMousePosition();
+
+            if (Input.GetMouseButtonUp(1))
+                RemoveTileAtMousePosition();
         }
 
         private void SetTileAtMousePosition()
         {
-            if (tilesCount >= tilesLimit) 
+            if (path.Count >= tilesLimit) 
                 return;
             
-            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePosition = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
             var tileLocation = pathTileMap.WorldToCell(mousePosition);
 
-            if (pathTileMap.GetTile(tileLocation))
+            if (pathTileMap.GetTile(tileLocation) || !IsLastTileNeighbor(tileLocation))
                 return;
             
             pathTileMap.SetTile(tileLocation, tile);
-            tilesCount++;
+            path.Add(pathTileMap.GetTile<Tile>(tileLocation));
+            tilesPositions.Add(tileLocation);
             UpdateAvailableTilesText();
+            
         }
 
-        private void IsValidTilePosition()
+        private void RemoveTileAtMousePosition()
         {
+            var mousePosition = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+            var tileLocation = pathTileMap.WorldToCell(mousePosition);
+
+            if (!pathTileMap.GetTile(tileLocation))
+                return;
             
+            var removedIndex = tilesPositions.FindIndex(position => position.Equals(tileLocation));
+            var lastIndex = tilesPositions.Count - 1;
+            var quantityToRemove = lastIndex - removedIndex;
+            for (var i = 0; i <= quantityToRemove; i++)
+            {
+                pathTileMap.SetTile(tilesPositions[lastIndex-i], null);
+                tilesPositions.RemoveAt(lastIndex-i);
+                path.RemoveAt(lastIndex-i);
+            }
+            UpdateAvailableTilesText();
         }
         
         private void UpdateAvailableTilesText()
         {
-            var updatedTilesCountTxt = $"Available tiles: {tilesLimit - tilesCount}";
+            var updatedTilesCountTxt = $"Available tiles: {tilesLimit - path.Count}";
             tilesCountTxt.text = updatedTilesCountTxt;
+            startWaveBtn.interactable = IsPathCompleted();
+        }
+
+        private bool IsLastTileNeighbor(Vector3Int tileLocation)
+        {
+            if (path.Count == 0) return true;
+
+            var lastTilePosition = tilesPositions.Last();
+
+            return IsNeighbor(lastTilePosition, tileLocation);
+        }
+
+        private bool IsPathCompleted()
+        {
+            if (tilesPositions.Count == 0) return false;
+            
+            var startPosition = new Vector3Int(-9, -1, 0);
+            var endPosition = new Vector3Int(8, -1, 0);
+
+            var firstTilePosition = tilesPositions.First();
+            var lastTilePosition = tilesPositions.Last();
+            
+            return IsNeighbor(startPosition, firstTilePosition) && IsNeighbor(endPosition, lastTilePosition);
+        }
+        
+        private static bool IsNeighbor(Vector3Int referencePosition, Vector3Int evaluatedPosition)
+        {
+            var isHorizontalNeighbor = 
+                (evaluatedPosition.x == referencePosition.x - 1 || evaluatedPosition.x == referencePosition.x + 1) && evaluatedPosition.y == referencePosition.y;
+            var isVerticalNeighbor = 
+                (evaluatedPosition.y == referencePosition.y - 1 || evaluatedPosition.y == referencePosition.y + 1) && evaluatedPosition.x == referencePosition.x;
+
+            return isHorizontalNeighbor || isVerticalNeighbor;
         }
     }
 }

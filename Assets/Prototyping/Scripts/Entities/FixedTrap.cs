268f9d340fc4e8dbec56a395fb66251af01b04c0
Vinícius Dantas <vdj@cin.ufpe.br>
2022-04-20 00:18:02 -0300

    adding trap classes

---
+++ Assets/Prototyping/Scripts/Entities/FixedTrap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototyping.Scripts.Entities
+{
+    public class FixedTrap : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+
---
+++ Assets/Prototyping/Scripts/Entities/TriggeredTrap.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototyping.Scripts.Entities
+{
+    public class TriggeredTrap : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
